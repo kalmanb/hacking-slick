f1c770d8c616a42daa5c9578fa6b91aa099101cc
kalmanb <kalman@movio.co>
2014-05-28 20:18:36 +1200

    First slick test

---
+++ src/test/scala/com/kalmanb/slick/FirstSlickTest.scala
@@ -0,0 +1,13 @@
+package com.kalmanb
+  
+import com.kalmanb.test.TestSpec
+import org.mockito.Matchers._
+       
+class FirstSlickTest extends TestSpec {
+
+  describe("a first test") {
+    it("should work") {
+      fail("oh no!")
+    }
+  }
+}
